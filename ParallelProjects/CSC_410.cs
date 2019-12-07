466cc80a394f4a6d310ebecd644b3edc7b55ddff
Dylan Teller <dylan.teller@trojans.dsu.edu>
2019-12-06 22:00:25 -0600

    Minor UI edits while running seed finder

--- ParallelProjects/CSC_410.cs
+++ ParallelProjects/CSC_410.cs
@@ -31,21 +31,25 @@ namespace ParallelProjects
                 seed_finder_button.Text = "Run";
                 seed_finder_textbox.Text = "Enter 5 numbers: ";
                 seed_finder_textbox.ReadOnly = false;
-            } else if (seed_finder_button.Text == "Run")
+            }
+            else if (seed_finder_button.Text == "Run")
             {
                 SeedFinderProgram seedFinder = new SeedFinderProgram();
                 string input = seed_finder_textbox.Text.Remove(0, 17);
-                seed_finder_button.Text = "Cancel";
+                seed_finder_button.Text = "Running...";
+                seed_finder_button.Enabled = false;
+                seed_finder_textbox.Text += Environment.NewLine + "Finding seed...";
+                seed_finder_textbox.ReadOnly = true;
                 var watch = System.Diagnostics.Stopwatch.StartNew();
                 var task = await Task.Run(() =>
                 {
                     return seedFinder.run(input);
                 });
                 watch.Stop();
+                seed_finder_button.Enabled = true;
                 var elapsedTime = (float)watch.ElapsedMilliseconds / 1000;
                 seed_finder_textbox.Text += Environment.NewLine + task + Environment.NewLine + elapsedTime.ToString() + " Seconds elapsed";
                 seed_finder_button.Text = "Start Seed Finder";
-                seed_finder_textbox.ReadOnly = true;
             }
         }
     }
